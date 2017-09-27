fb13a40f1921ecf5a8ab156f1bc01a43fc082339
rileylee <liyizhou@me.com>
2017-09-27 23:36:29 +0800

    add Day09

--- Day09_Extentions01.playground/Contents.swift
+++ Day09_Extentions01.playground/Contents.swift
@@ -15,3 +15,36 @@ extension String {
 
 let name = "Lee"
 name.reverse()
+
+extension Int{
+    mutating func square() -> Int {
+        self = self * self
+        return self
+    }
+}
+
+var value = 9
+value.square()
+print(value)
+
+//Mutating
+extension Double {
+    mutating func calculateArea() {
+        let pi = 3.1415
+        self = pi * (self * self)
+    }
+}
+
+class Circle {
+    var radius: Double
+    
+    init(radius: Double) {
+        self.radius = radius
+    }
+}
+
+var circle = Circle(radius: 3.3)
+circle.radius
+circle.radius.calculateArea()
+circle.radius
+
